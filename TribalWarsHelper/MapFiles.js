76737532752735340689226ff5361b3fd5ac9274
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2017-12-29 23:44:14 +0100

    faking and mapfiles release versions

--- TribalWarsHelper/MapFiles.js
+++ TribalWarsHelper/MapFiles.js
@@ -2,48 +2,62 @@
  * Helper for downloading world info in tribalwars - browser game
  */
 
+
 function GetWorldInfo() {
+    let debugMode = game_data.player.id === '699198069';
+    let _regex = new RegExp(/\+/, 'g');
     let world = game_data.world;
-    let TribalWarsHelper = {};
+    let startTime = Date.now();
+    if (debugMode)
+        console.log("Start time:", startTime);
     return Promise.all([
-        DownloadData(world, 'map/player.txt').then(text => {
+        DownloadData(world, 'map/player.txt', 2).then(text => {
             return Parser(text, ParsePlayer);
         }),
-        DownloadData(world, 'map/ally.txt').then(text => {
+        DownloadData(world, 'map/ally.txt', 6).then(text => {
             return Parser(text, ParseAlly);
         }),
-        DownloadData(world, 'map/village.txt').then(text => {
+        DownloadData(world, 'map/village.txt', 6).then(text => {
             return Parser(text, ParseVillage);
         }),
-        DownloadData(world, 'interface.php?func=get_config').then(text => {
+        DownloadData(world, 'interface.php?func=get_config', 48).then(text => {
             return Parser(text);
         }),
-        DownloadData(world, 'interface.php?func=get_building_info').then(text => {
+        DownloadData(world, 'interface.php?func=get_building_info', 48).then(text => {
             return Parser(text);
         }),
-        DownloadData(world, 'interface.php?func=get_unit_info').then(text => {
+        DownloadData(world, 'interface.php?func=get_unit_info', 48).then(text => {
             return Parser(text);
         })
 
     ]).then(results => {
-        TribalWarsHelper['player'] = results[0];
-        TribalWarsHelper['ally'] = results[1];
-        TribalWarsHelper['village'] = results[2];
-        TribalWarsHelper['config'] = results[3];
-        TribalWarsHelper['buildings'] = results[4];
-        TribalWarsHelper['units'] = results[5];
-        return TribalWarsHelper;
+        let endTime = Date.now();
+        if (debugMode) {
+            console.log("End time:", endTime);
+            console.log("Execution time:", endTime - startTime);
+        }
+        return {
+            'player': results[0],
+            'ally': results[1],
+            'village': results[2],
+            'config': {
+                'general': results[3],
+                'building': results[4],
+                'unit': results[5]
+            }
+        };
     }).catch(e => {
         Dialog.show('scriptError', `<h2>Blad podczas wykonywania skryptu</h2><p>Komunikat o bledzie: <br/><textarea>${e}</textarea></p>`)
     });
 
-    function DownloadData(server, path) {
+    function DownloadData(server, path, expirationTime) {
         let timestampKey = `${server}${path}${'timestamp'}`;
         let dataKey = `${server}${path}${'data'}`;
         let timestamp = localStorage[timestampKey];
-        if (timestamp === undefined || (timestamp + 3600 * 1000) < Date.now()) {
+        if (timestamp === undefined || (timestamp + 3600 * 1000 * expirationTime) < Date.now()) {
             return fetch(`https://${server}.plemiona.pl/${path}`).then(t => t.text()).then(text => {
-                console.log('fetching over network');
+                if (debugMode)
+                    console.log('fetching over network');
                 let content = text;
                 localStorage[timestampKey] = Date.now();
                 localStorage[dataKey] = content;
@@ -51,14 +65,14 @@ function GetWorldInfo() {
             });
         }
         else {
-            return new Promise(function (resolve, reject) {
-                console.log('fetching over cache');
+            return new Promise(function (resolve) {
+                if (debugMode)
+                    console.log('fetching over cache');
                 let data = localStorage[dataKey];
                 if (data === undefined) {
-                    reject();
                     localStorage.removeItem(dataKey);
                     localStorage.removeItem(timestampKey);
-                    return DownloadData(server, path);
+                    return resolve(DownloadData(server, path));
                 }
                 resolve(data);
             });
@@ -82,7 +96,7 @@ function GetWorldInfo() {
         let raw = rawLine.split(',');
         return {
             id: Number(raw[0]),
-            name: decodeURI(raw[1]).replace(new RegExp(/\+/, 'g'), ' '),
+            name: _decode(raw[1]),
             allyId: Number(raw[2]),
             villages: Number(raw[3]),
             points: Number(raw[4]),
@@ -94,8 +108,8 @@ function GetWorldInfo() {
         let raw = rawLine.split(',');
         return {
             id: Number(raw[0]),
-            name: decodeURI(raw[1].replace(new RegExp(/\+/, 'g'), ' ')),
-            tag: decodeURI(raw[2].replace(new RegExp(/\+/, 'g'), ' ')),
+            name: _decode(raw[1]),
+            tag: _decode(raw[2]),
             playersCount: Number(raw[3]),
             villagesCount: Number(raw[4]),
             top40Points: Number(raw[5]),
@@ -108,7 +122,7 @@ function GetWorldInfo() {
         let raw = rawLine.split(',');
         return {
             id: Number(raw[0]),
-            name: decodeURI(raw[1].replace(new RegExp(/\+/, 'g'), ' ')),
+            name: _decode(raw[1]),
             x: Number(raw[2]),
             y: Number(raw[3]),
             coords: `${Number(raw[2])}|${Number(raw[3])}`,
@@ -151,4 +165,7 @@ function GetWorldInfo() {
         return obj;
     }
 
+    function _decode(encodedString) {
+        return decodeURIComponent(encodedString.replace(_regex, ' '));
+    }
 }
