e3f7162012223ceb9809df62634116c2bb76a278
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2020-09-27 17:59:21 +0200

    remove other awards

--- userscripts/HermitowskieBlaszki.userscript.js
+++ userscripts/HermitowskieBlaszki.userscript.js
@@ -14,7 +14,7 @@
     'use strict';
 
     const player_id = ''; // Ustaw na swój id jeżeli chcesz widzieć blaszki również z poziomu dostępu gościnnego
-    const hide_others_awards = true;
+    const remove_awards_from_other_profiles = true;
 
     // Dostosuj wpisy według własnego uznania
     const awards = [
@@ -31,40 +31,62 @@
 
     const HermitowskieBlaszki = {
         init: function () {
-            if (HermitowskieBlaszki.check_screen()) {
-                const award_group = HermitowskieBlaszki.get_or_create_award_group_victory_achievements();
-                for (const award of awards) {
-                    HermitowskieBlaszki.create_award_box(award_group, award);
+            const url_params = new URLSearchParams(window.location.search);
+            const target_player_id = (typeof (game_data) !== "undefined" && typeof (game_data.player) !== "undefined" && typeof (game_data.player.id) !== "undefined")
+                ? game_data.player.id
+                : player_id;
+            if (url_params.get('screen') === 'info_player') {
+                if (url_params.get('id') == target_player_id) {
+                    const award_group = HermitowskieBlaszki.get_or_create_award_group_victory_achievements();
+                    for (const award of awards) {
+                        HermitowskieBlaszki.create_award_box(award_group, award);
+                    }
+                }
+                else if (remove_awards_from_other_profiles) {
+                    const award_group = HermitowskieBlaszki.get_or_create_award_group_victory_achievements();
+                    award_group.remove();
                 }
             }
         },
         get_container: function () {
+            const award_group = document.querySelector('.award-group');
+            if (award_group) {
+                return award_group.parentNode;
+            }
             switch (document.location.pathname) {
                 case '/guest.php':
-                    return document.querySelector('#content_value > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(11) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)');
+                    return document.querySelector('#content_value > table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(11) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)');
                 case '/pregame.php':
                 case '/game.php':
-                    return document.querySelector('#content_value > table:nth-child(7) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)');
+                    return document.querySelector('#content_value > table > tbody > tr:nth-child(1) > td:nth-child(2)');
             }
             return null;
         },
         get_or_create_award_group_victory_achievements: function () {
-            const award_group_template = `
-            <div class="award-group-head">${i18n.VICTORY_ACHIEVEMENTS}</div>
-            <div class="award-group-content"></div>
-            <div class="award-group-foot">&nbsp;</div>`;
-            const container = HermitowskieBlaszki.get_container();
-
-            const award_groups_existing = container.querySelectorAll('.award-group');
+            const award_groups_existing = document.querySelectorAll('.award-group');
 
             if (award_groups_existing.length && award_groups_existing[0].children[0].innerText === i18n.VICTORY_ACHIEVEMENTS) {
                 return award_groups_existing[0];
             }
 
+            const container = HermitowskieBlaszki.get_container();
             const award_group_new = document.createElement('div');
+            const award_group_template = `
+            <div class="award-group-head">${i18n.VICTORY_ACHIEVEMENTS}</div>
+            <div class="award-group-content"></div>
+            <div class="award-group-foot">&nbsp;</div>`;
             award_group_new.innerHTML = award_group_template;
             award_group_new.classList.add('award-group');
-            container.prepend(award_group_new);
+            let i = 0;
+            for (; i < container.children.length; i++) {
+                if (container.children[i].classList.contains('award-group')) {
+                    container.insertBefore(award_group_new, container.children[i]);
+                    break;
+                }
+            }
+            if (i == container.children.length) {
+                container.append(award_group_new);
+            }
             return award_group_new;
         },
         create_award_box: function (award_group, award) {
@@ -84,13 +106,6 @@
             const award_group_content = award_group.querySelector('.award-group-content');
             award_group_content.append(award_box);
         },
-        check_screen: function () {
-            const url_params = new URLSearchParams(window.location.search);
-            const target_player_id = (typeof (game_data) !== "undefined" && typeof (game_data.player) !== "undefined" && typeof (game_data.player.id) !== "undefined")
-                ? game_data.player.id
-                : player_id;
-            return url_params.get('screen') === 'info_player' && url_params.get('id') == target_player_id;
-        }
     };
 
     HermitowskieBlaszki.init();
