559f6e6e0e7851399baa1d6e72d19643db09e5dd
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2022-10-19 21:58:42 +0200

    Faking: fixed retrieving configuration from forum

--- scripts/src/inf/TribalWars.ts
+++ scripts/src/inf/TribalWars.ts
@@ -1,6 +1,6 @@
 export interface ITribalWars {
     buildURL(method: "POST" | "GET", screen: string, args: { [name: string]: string }): string;
-    fetchDocument(method: "POST" | "GET", screen: string, args: { [name: string]: string | number }): Promise<Document>;
+    fetchDocument(method: "POST" | "GET", screen: string, args: { [name: string]: string | number }): Promise<HTMLElement>;
     fetchJSON(method: "POST" | "GET", screen: string, args: { [name: string]: string }): Promise<object>;
     getGameData(): GameData;
 }
@@ -30,13 +30,13 @@ export class TribalWarsProvider implements ITribalWars {
         return TribalWars.buildURL(method, screen, args);
     }
 
-    async fetchDocument(method: "POST" | "GET", screen: string, args: { [name: string]: string; }): Promise<Document> {
+    async fetchDocument(method: "POST" | "GET", screen: string, args: { [name: string]: string; }): Promise<HTMLElement> {
         const url = this.buildURL(method, screen, args);
         const response = await fetch(url);
         const text = await response.text();
         const doc = document.createElement("document");
         doc.innerHTML = text;
-        return document;
+        return doc;
     }
 
     async fetchJSON(method: "POST" | "GET", screen: string, args: { [name: string]: string; }): Promise<object> {
